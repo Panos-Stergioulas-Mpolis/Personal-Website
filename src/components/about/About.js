3d3b7d7e3c1a73c43d73bb315c5fdd1118683e8d
Panos-Stergioulas-Mpolis <panosstergioulasmpolis@gmail.com>
2023-04-13 00:18:44 +0300

    About

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,58 +1,68 @@
 import React from 'react'
 
+
 function About() {
   return (
-    <div className='ml-[50px] mt-[130px] w-[60%]'>
-        <div className='text-3xl text-[#642db7] font-bold'>
-        &lt;About&gt;
-        <div className='ml-[30px] my-1'>
-            <div className='text-xl  font-bold  text-black'>
-            &lt;Education&gt;
-            </div>
-                <p className='ml-[30px] my-1 text-lg text-black font-normal '>
-                ry to popular belief, Lorem Ipsum is not simply random 
-            text. Itt Hampden-Sydney 
-            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem
-             Ipsum passage, and going through the cites of the word in classical literature, discovered the
-                </p>
-             <div className='text-xl  font-bold  text-black'>
-             &lt;/Education&gt;
-             </div>
-        </div>
-        <div className='ml-[30px] my-1'>
-            <div className='text-xl  font-bold  text-black'>
-            &lt;Skills&gt;
-            </div>
-                <p className='ml-[30px] my-1 text-lg text-black font-normal '>
-                ry to popular belief, Lorem Ipsum is not simply random 
-            text. Itt Hampden-Sydney 
-            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem
-             Ipsum passage, and going through the cites of the word in classical literature, discovered the
-                </p>
-             <div className='text-xl  font-bold  text-black'>
-             &lt;/Skills&gt;
-             </div>
-        </div>
-        <div className='ml-[30px] my-1'>
-            <div className='text-xl  font-bold  text-black'>
-            &lt;What I Do&gt;
-            </div>
-                <p className='ml-[30px] my-1 text-lg text-black font-normal '>
-                ry to popular belief, Lorem Ipsum is not simply random 
-            text. Itt Hampden-Sydney 
-            College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem
-             Ipsum passage, and going through the cites of the word in classical literature, discovered the
-                </p>
-             <div className='text-xl  font-bold text-black'>
-             &lt;/What I Do&gt;
-             </div>
-        </div>
-        </div>
+    <div className='mt-[150px] w-full px-[80px] flex justify-center flex-wrap gap-[60px]'>
 
-        <div className='text-3xl text-[#642db7] font-bold'>
-        &lt;/About&gt;
-        </div>
-        
+            <div className='w-[300px] shadow-[0_10px_10px_rgba(0,0,0,0.3)] h-[400px] text-center flex flex-col gap-6 '>
+
+                <h1 className='font-semibold text-[25px] text-center text-white bg-[#642db7]'>Education</h1>
+                <div className='flex flex-col gap-6'>
+                    <p className='w-[70%] m-auto'>
+                        I study at the University of Macedonia in the 
+                        department of applied informatics and information systems
+                    </p>
+                    <a href='https://www.uom.gr/' target='_blank'>
+                        <img alt="uom" src='https://www.uom.gr/site/images/logo-new.png' className='w-[200px]'></img>
+                    </a>
+                </div>
+
+            </div>
+
+            <div className='w-[300px] shadow-[0_10px_10px_rgba(0,0,0,0.3)] h-[400px] flex flex-col gap-6 '>
+
+                <h1 className='font-semibold text-[25px] text-center text-white bg-[#642db7]'>Soft Skills</h1>
+                <div>
+                    <ul className='w-[70%] m-auto gap-3 flex flex-col text-[20px] list-disc'>
+                        <li>Problem-solving</li>
+                        <li>Leadership</li>
+                        <li>Communication</li>
+                        <li>Collaboration</li>
+                        <li>Adaptability</li>
+                        <li>Time management</li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className='w-[300px] shadow-[0_10px_10px_rgba(0,0,0,0.3)] h-[400px] flex flex-col gap-6 '>
+
+                <h1 className='font-semibold text-[25px] text-center text-white bg-[#642db7]'>Languages</h1>
+                <div>
+                    <ul className='w-[70%] m-auto gap-[25px] items-center flex flex-col text-[30px]'>
+                        <li>English</li>
+                        <li>Greek</li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div className='w-[90%] shadow-[0_10px_10px_rgba(0,0,0,0.3)] h-[400px] flex flex-col gap-6 '>
+
+                <h1 className='font-semibold text-[25px] text-center text-white bg-[#642db7]'>Programming languages</h1>
+                <div>
+                    <ul className='w-full m-auto items-center flex flex-col '>
+                        <li>JavaScript</li>
+                        <li>Java</li>
+                        <li>C</li>
+                        <li>Python</li>
+                        <li>Java</li>
+                    </ul>
+                </div>
+
+            </div>
+
     </div>
   )
 }
